3b3e9318d25b5d55eb05b2b5a79eb02543b57446
elhady <i.elhady44@gmail.com>
2023-08-22 21:45:33 +0300

    hilt view model error

--- app/src/main/java/com/elhady/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/home/HomeViewModel.kt
@@ -2,8 +2,9 @@ package com.elhady.movies.ui.home
 
 import androidx.lifecycle.ViewModel
 import com.elhady.movies.data.remote.repository.MovieRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-//@HiltViewModel
+@HiltViewModel
 class HomeViewModel @Inject  constructor(private val repository: MovieRepository)  : ViewModel() {
 }
