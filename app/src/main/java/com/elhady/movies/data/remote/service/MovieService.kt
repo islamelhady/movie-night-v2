27b84a4e602ed7f53dcd34da31b0ba2285d3d8e7
elhady <i.elhady44@gmail.com>
2023-08-22 16:18:55 +0300

    add network module

--- app/src/main/java/com/elhady/movies/data/remote/service/MovieService.kt
+++ app/src/main/java/com/elhady/movies/data/remote/service/MovieService.kt
@@ -1,12 +1,7 @@
 package com.elhady.movies.data.remote.service
 
-import com.elhady.movies.data.remote.AuthInterceptor
 import com.elhady.movies.data.remote.response.MovieResponse
-import com.elhady.movies.utilities.Constants
-import okhttp3.OkHttpClient
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface MovieService {
@@ -25,14 +20,3 @@ interface MovieService {
 
 }
 
-object ApiRequest {
-    private val client = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .client(client)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val service = retrofit.create(MovieService::class.java)
-}
---
+++ app/src/main/java/com/elhady/movies/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.elhady.movies.di
+
+import com.elhady.movies.data.remote.AuthInterceptor
+import com.elhady.movies.data.remote.service.MovieService
+import com.elhady.movies.utilities.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieService(
+        client: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): MovieService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+
+        return retrofit.create(MovieService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: AuthInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(): AuthInterceptor {
+        return AuthInterceptor()
+    }
+}
