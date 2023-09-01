121c692a05f29572897bca387318885f076e2c58
elhady <i.elhady44@gmail.com>
2023-09-01 05:34:27 +0300

    handle login ViewModel

--- app/src/main/java/com/elhady/movies/ui/login/LoginFragment.kt
+++ app/src/main/java/com/elhady/movies/ui/login/LoginFragment.kt
@@ -1,32 +1,15 @@
 package com.elhady.movies.ui.login
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.elhady.movies.R
+import com.elhady.movies.databinding.FragmentLoginBinding
+import com.elhady.movies.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginFragment : Fragment() {
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val layoutIdFragment: Int = R.layout.fragment_login
+    override val viewModel: LoginViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
-    private lateinit var viewModel: LoginViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/com/elhady/movies/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/login/LoginViewModel.kt
@@ -1,7 +1,26 @@
 package com.elhady.movies.ui.login
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.elhady.movies.data.repository.AccountRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val accountRepository: AccountRepository): ViewModel() {
+
+    private val _username = MutableLiveData("")
+    private val _password = MutableLiveData("")
+
+    fun onUsernameChanged(userName: String){
+        _username.postValue(userName)
+    }
+
+    fun onPasswordChanged(password: String){
+        _password.postValue(password)
+    }
+
+    suspend fun loginWithUsernameAndPassword(userName: String, password: String){
+        accountRepository.loginWithUsernameAndPassword(userName, password)
+    }
 }
