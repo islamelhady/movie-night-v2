528f0c4bc0e78296c542af31023babac1be7eef8
elhady <i.elhady44@gmail.com>
2023-09-01 08:35:08 +0300

    login view model functions

--- app/src/main/java/com/elhady/movies/ui/login/LoginFragment.kt
+++ app/src/main/java/com/elhady/movies/ui/login/LoginFragment.kt
@@ -1,9 +1,13 @@
 package com.elhady.movies.ui.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.elhady.movies.R
 import com.elhady.movies.databinding.FragmentLoginBinding
 import com.elhady.movies.ui.base.BaseFragment
+import com.elhady.movies.utilities.EventObserve
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,5 +15,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override val layoutIdFragment: Int = R.layout.fragment_login
     override val viewModel: LoginViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.loginEvent.observe(viewLifecycleOwner, EventObserve {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        })
+    }
 
 }
--- app/src/main/java/com/elhady/movies/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/login/LoginViewModel.kt
@@ -1,26 +1,82 @@
 package com.elhady.movies.ui.login
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.elhady.movies.data.remote.State
 import com.elhady.movies.data.repository.AccountRepository
+import com.elhady.movies.utilities.Event
+import com.elhady.movies.utilities.TextValidation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val accountRepository: AccountRepository): ViewModel() {
+class LoginViewModel @Inject constructor(private val accountRepository: AccountRepository, val textValidation: TextValidation): ViewModel() {
 
-    private val _username = MutableLiveData("")
-    private val _password = MutableLiveData("")
+    val userName = MutableLiveData("")
+    val password = MutableLiveData("")
 
-    fun onUsernameChanged(userName: String){
-        _username.postValue(userName)
+    private val _passwordHelperText = MutableLiveData("")
+    val passwordHelperText: LiveData<String> = _passwordHelperText
+
+    private val _userNameHelperText = MutableLiveData("")
+    val userNameHelperText: LiveData<String> = _userNameHelperText
+
+    private val _loginRequestState = MutableLiveData<State<Boolean>>()
+    val loginRequestState: LiveData<State<Boolean>> = _loginRequestState
+
+    private val _loginEvent = MutableLiveData<Event<Boolean>>()
+    val loginEvent: LiveData<Event<Boolean>> = _loginEvent
+
+
+    fun onClickLogin() {
+        if (isValidUserName() && isValidPassword()) {
+            whenFormIsValid()
+        }
     }
 
-    fun onPasswordChanged(password: String){
-        _password.postValue(password)
+    private fun isValidPassword(): Boolean {
+        val validPassword = textValidation.validPassword(password.value.toString())
+        _passwordHelperText.postValue(validPassword)
+        return validPassword == null
     }
 
-    suspend fun loginWithUsernameAndPassword(userName: String, password: String){
-        accountRepository.loginWithUsernameAndPassword(userName, password)
+    private fun isValidUserName(): Boolean {
+        val validUserName = textValidation.validFiled(userName.value.toString())
+        _userNameHelperText.postValue(validUserName)
+        return validUserName == null
     }
+
+
+    private fun whenFormIsValid() {
+        login()
+    }
+
+    fun login() {
+        viewModelScope.launch {
+            accountRepository.loginWithUsernameAndPassword(userName.value.toString(), password.value.toString())
+        }
+    }
+
+    private fun onLoginSuccessfully() {
+        _loginRequestState.postValue(State.Success(true))
+        _loginEvent.postValue(Event(true))
+        resetForm()
+
+    }
+
+    private fun onLoginError(message: String) {
+        _loginRequestState.postValue(State.Error(message))
+        _passwordHelperText.postValue(message)
+
+    }
+
+
+    private fun resetForm() {
+        userName.postValue(null)
+        password.postValue(null)
+    }
+
 }
