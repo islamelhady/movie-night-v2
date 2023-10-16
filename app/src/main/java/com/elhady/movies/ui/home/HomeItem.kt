249beaae2b621d5e88809f2c6152e1387047aa04
elhady <i.elhady44@gmail.com>
2023-10-16 18:59:02 +0300

    arrange adapter priority

--- app/src/main/java/com/elhady/movies/ui/home/HomeItem.kt
+++ app/src/main/java/com/elhady/movies/ui/home/HomeItem.kt
@@ -5,16 +5,16 @@ import com.elhady.movies.ui.models.ActorUiState
 import com.elhady.movies.ui.models.PopularUiState
 import com.elhady.movies.ui.models.MediaUiState
 
-sealed interface HomeItem{
-    data class Slider(val items: List<PopularUiState>): HomeItem
-    data class Upcoming(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.UPCOMING): HomeItem
-    data class Trending(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.TRENDING): HomeItem
-    data class NowPlaying(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.NOW_PLAYING): HomeItem
-    data class TopRated(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.TOP_RATED): HomeItem
-    data class OnTheAirSeries(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.ON_THE_AIR_SERIES): HomeItem
-    data class AiringTodaySeries(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.AIRING_TODAY_SERIES): HomeItem
-    data class TVSeriesLists(val items: List<MediaUiState>): HomeItem
-    data class Mystery(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.MYSTERY): HomeItem
-    data class Adventure(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.ADVENTURE): HomeItem
-    data class Actor(val items: List<ActorUiState>): HomeItem
+sealed class HomeItem (val priority: Int){
+    data class Slider(val items: List<PopularUiState>): HomeItem(0)
+    data class Upcoming(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.UPCOMING): HomeItem(1)
+    data class Trending(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.TRENDING): HomeItem(2)
+    data class TVSeriesLists(val items: List<MediaUiState>): HomeItem(3)
+    data class NowPlaying(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.NOW_PLAYING): HomeItem(4)
+    data class OnTheAirSeries(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.ON_THE_AIR_SERIES): HomeItem(5)
+    data class TopRated(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.TOP_RATED): HomeItem(6)
+    data class AiringTodaySeries(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.AIRING_TODAY_SERIES): HomeItem(7)
+    data class Mystery(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.MYSTERY): HomeItem(8)
+    data class Adventure(val items: List<MediaUiState>, val type: HomeItemType = HomeItemType.ADVENTURE): HomeItem(9)
+    data class Actor(val items: List<ActorUiState>): HomeItem(10)
 }
--- app/src/main/java/com/elhady/movies/ui/home/adapters/HomeAdapter.kt
+++ app/src/main/java/com/elhady/movies/ui/home/adapters/HomeAdapter.kt
@@ -3,6 +3,7 @@ package com.elhady.movies.ui.home.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.elhady.movies.BR
 import com.elhady.movies.R
 import com.elhady.movies.ui.base.BaseAdapter
@@ -15,6 +16,13 @@ class HomeAdapter(
 ) :
     BaseAdapter<HomeItem>(homeItems, listener) {
     override val layoutID: Int = 0
+
+    override fun setItems(newItems: List<HomeItem>) {
+        homeItems = newItems.sortedBy {
+            it.priority
+        }.toMutableList()
+        super.setItems(newItems)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         return ItemViewHolder(
@@ -123,10 +131,6 @@ class HomeAdapter(
         }
     }
 
-    override fun setItems(newItems: List<HomeItem>) {
-        homeItems = newItems.toMutableList()
-        super.setItems(newItems)
-    }
 
 
     override fun areItemContent(oldItem: HomeItem, newItem: HomeItem): Boolean {
