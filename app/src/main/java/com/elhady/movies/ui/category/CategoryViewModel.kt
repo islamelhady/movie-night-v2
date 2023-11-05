c83217c678d61f488d02757fc045144be76a3832
elhady <i.elhady44@gmail.com>
2023-11-06 01:33:23 +0200

    text view model with adventure id genre

--- app/src/main/java/com/elhady/movies/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/category/CategoryViewModel.kt
@@ -1,19 +1,41 @@
 package com.elhady.movies.ui.category
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.paging.map
+import com.elhady.movies.domain.usecases.GetMoviesByGenreIDUseCase
 import com.elhady.movies.ui.base.BaseViewModel
+import com.elhady.movies.ui.mappers.MediaUiMapper
+import com.elhady.movies.ui.models.MediaUiState
+import com.elhady.movies.utilities.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryViewModel @Inject constructor() : BaseViewModel() {
-    // TODO: Implement the ViewModel
+class CategoryViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val getMoviesByGenreIDUseCase: GetMoviesByGenreIDUseCase,
+    private val mediaUiMapper: MediaUiMapper
+) : BaseViewModel() {
+
+    val args = CategoryFragmentArgs.fromSavedStateHandle(savedStateHandle)
+
 
     private val _categoryUiState = MutableStateFlow(CategoryUiState())
     val categoryUiState = _categoryUiState.asStateFlow()
 
     override fun getData() {
-        TODO("Not yet implemented")
+        val result = getMoviesByGenreIDUseCase(genreId = Constants.ADVENTURE_ID).map { pagingData ->
+            pagingData.map {
+                mediaUiMapper.map(it)
+            }
+        }
+        _categoryUiState.update {
+            it.copy(moviesResult = result)
+        }
     }
+
 }
