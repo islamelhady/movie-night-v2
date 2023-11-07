2faaaf2c0ae23a4b0ab5032da13a1aea0a7d5a7b
elhady <i.elhady44@gmail.com>
2023-11-08 00:13:29 +0200

    add search ui state

---
+++ app/src/main/java/com/elhady/movies/ui/search/SearchUiState.kt
@@ -0,0 +1,11 @@
+package com.elhady.movies.ui.search
+
+import androidx.paging.PagingData
+import com.elhady.movies.ui.models.MediaUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class SearchUiState(
+    val inputSearch: String = "",
+    val moviesSearchResult: Flow<PagingData<MediaUiState>> = emptyFlow()
+)
--- app/src/main/java/com/elhady/movies/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/search/SearchViewModel.kt
@@ -1,13 +1,43 @@
 package com.elhady.movies.ui.search
 
+import androidx.paging.map
+import com.elhady.movies.domain.mappers.movie.MovieDtoMapper
+import com.elhady.movies.domain.usecases.search.GetSearchForMovieUseCase
 import com.elhady.movies.ui.base.BaseViewModel
+import com.elhady.movies.ui.mappers.MediaUiMapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(): BaseViewModel() {
-    // TODO: Implement the ViewModel
+class SearchViewModel @Inject constructor(
+    private val searchForMovieUseCase: GetSearchForMovieUseCase,
+    private val mediaUiMapper: MediaUiMapper
+) : BaseViewModel() {
+
+    private val _searchUiState = MutableStateFlow(SearchUiState())
+    val searchUiState = _searchUiState.asStateFlow()
+
+
+
     override fun getData() {
-        TODO("Not yet implemented")
+        onSearchForMovies(searchUiState.value.inputSearch)
+
     }
+
+    private fun onSearchForMovies(movie: String) {
+        val result = searchForMovieUseCase(movieQuery = movie).map { pagingData ->
+            pagingData.map {
+                mediaUiMapper.map(it)
+            }
+        }
+        _searchUiState.update {
+            it.copy(moviesSearchResult = result)
+        }
+    }
+
+
 }
