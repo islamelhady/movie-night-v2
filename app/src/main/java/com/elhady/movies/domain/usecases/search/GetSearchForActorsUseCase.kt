2148a1af93f405f4bfc2183b858625e88ef5dd2f
elhady <i.elhady44@gmail.com>
2023-11-11 04:46:17 +0200

    feat: add actors search use case

--- app/src/main/java/com/elhady/movies/domain/usecases/search/GetSearchForActorsUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/search/GetSearchForActorsUseCase.kt
@@ -1,16 +1,22 @@
 package com.elhady.movies.domain.usecases.search
 
 import androidx.paging.PagingData
-import com.elhady.movies.data.local.mappers.actors.ActorsMapper
+import androidx.paging.map
 import com.elhady.movies.data.repository.ActorRepository
+import com.elhady.movies.domain.mappers.search.ActorsDtoMapper
 import com.elhady.movies.domain.models.Media
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class GetSearchForActorsUseCase constructor(private val repository: ActorRepository, private val mediamap) {
-    suspend operator fun invoke(query: String): Flow<PagingData<Media>>{
-        repository.searchForActors(query).flow.map {
-
+class GetSearchForActorsUseCase constructor(
+    private val repository: ActorRepository,
+    private val actorsDtoMapper: ActorsDtoMapper
+) {
+    suspend operator fun invoke(query: String): Flow<PagingData<Media>> {
+        return repository.searchForActors(query).flow.map { pagingData ->
+            pagingData.map {
+                actorsDtoMapper.map(it)
+            }
         }
     }
 }
