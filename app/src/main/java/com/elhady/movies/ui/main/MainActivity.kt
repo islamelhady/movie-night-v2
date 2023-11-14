cc1e0db91625f5520169a2896d60bb699b69e2a5
elhady <i.elhady44@gmail.com>
2023-11-14 09:11:22 +0200

    update main activity

--- app/src/main/java/com/elhady/movies/ui/main/MainActivity.kt
+++ app/src/main/java/com/elhady/movies/ui/main/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.elhady.movies.ui.main
 
 import android.os.Bundle
+import android.view.Window
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.elhady.movies.R
@@ -18,23 +23,46 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY)
+        setTheme(R.style.Theme_Movies)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
-        setSupportActionBar(binding.toolbar)
-        val navView: BottomNavigationView = binding.navView
-
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+    override fun onResume() {
+        super.onResume()
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.homeFragment, R.id.exploreFragment, R.id.favoriteFragment, R.id.profileFragment
+                R.id.homeFragment,
+                R.id.exploreFragment,
+                R.id.favoriteFragment,
+                R.id.profileFragment,
             )
         )
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.setupWithNavController(navController)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        setBottomNavigationVisibility(navController)
+        setNavigationController(navController)
+    }
+
+    private fun setBottomNavigationVisibility(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.navView.isVisible = destination.id != R.id.loginFragment
+        }
+    }
+
+    private fun setNavigationController(navController: NavController) {
+        binding.navView.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navController)
+            navController.popBackStack(item.itemId, inclusive = false)
+            true
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment_activity_main).navigateUp() || super.onSupportNavigateUp()
     }
 }
