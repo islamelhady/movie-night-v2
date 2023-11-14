63583c4baaa05ae41fdb6aad43d455ff994cc121
elhady <i.elhady44@gmail.com>
2023-11-14 07:20:25 +0200

    feat: add movie trailer use case

--- app/src/main/java/com/elhady/movies/domain/usecases/GetTrailerUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/GetTrailerUseCase.kt
@@ -1,10 +1,18 @@
 package com.elhady.movies.domain.usecases
 
 import com.elhady.movies.data.repository.MovieRepository
+import com.elhady.movies.domain.mappers.TrailerMapper
 import com.elhady.movies.domain.models.Trailer
+import javax.inject.Inject
 
-class GetTrailerUseCase constructor(private val movieRepository: MovieRepository) {
+class GetTrailerUseCase @Inject constructor(
+    private val movieRepository: MovieRepository,
+    private val trailerMapper: TrailerMapper
+) {
     suspend operator fun invoke(movieId: Int): Trailer {
-        return movieRepository.getMovieTrailer(movieId)
+        val response = movieRepository.getMovieTrailer(movieId)
+        return response?.let {
+            trailerMapper.map(it)
+        } ?: throw Throwable("error")
     }
 }
