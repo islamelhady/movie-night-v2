e60062c25cae0de06bf0afa990fd962464b302f4
elhady <i.elhady44@gmail.com>
2023-11-15 03:22:07 +0200

    add profile ui state

--- app/src/main/java/com/elhady/movies/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,20 @@
 package com.elhady.movies.ui.profile
 
-import androidx.lifecycle.ViewModel
-import com.elhady.movies.data.repository.MovieRepository
+import com.elhady.movies.domain.usecases.CheckIfLoggedInUseCase
+import com.elhady.movies.ui.base.BaseViewModel
+import com.elhady.movies.ui.models.MediaUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject  constructor(private val repository: MovieRepository)  : ViewModel() {
+class ProfileViewModel @Inject constructor(private val checkIfLoggedInUseCase: CheckIfLoggedInUseCase)  : BaseViewModel() {
+
+    private val _profileUiState = MediaUiState()
+
+    init {
+        getData()
+    }
+    override fun getData() {
+        TODO("Not yet implemented")
+    }
 }
