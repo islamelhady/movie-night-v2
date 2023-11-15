5bfcaf24627e763b8a38204234c8c01f077691d6
elhady <i.elhady44@gmail.com>
2023-11-15 07:21:34 +0200

    add insert movie use case

--- app/src/main/java/com/elhady/movies/domain/usecases/movieDetails/InsertMoviesUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/movieDetails/InsertMoviesUseCase.kt
@@ -1,10 +1,16 @@
 package com.elhady.movies.domain.usecases.movieDetails
 
 import com.elhady.movies.data.repository.MovieRepository
+import com.elhady.movies.domain.mappers.movie.WatchHistoryMapper
 import com.elhady.movies.domain.models.MovieDetails
+import javax.inject.Inject
 
-class InsertMoviesUseCase constructor(private val movieRepository: MovieRepository) {
-    operator fun invoke(movie: MovieDetails){
-        movieRepository.insertMovieWatch()
+class InsertMoviesUseCase @Inject constructor(
+    private val movieRepository: MovieRepository,
+    private val watchHistoryMapper: WatchHistoryMapper
+) {
+    suspend operator fun invoke(movie: MovieDetails) {
+        val movieWatch = watchHistoryMapper.map(movie)
+        movieRepository.insertMovieWatch(movieWatch)
     }
 }
