59127cb0600286e3364ad1958d43ef736a917e25
elhady <i.elhady44@gmail.com>
2023-11-15 07:57:36 +0200

    add watch ui state mapper

--- app/src/main/java/com/elhady/movies/ui/profile/watchHistory/WatchHistoryViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/profile/watchHistory/WatchHistoryViewModel.kt
@@ -1,11 +1,14 @@
 package com.elhady.movies.ui.profile.watchHistory
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.elhady.movies.domain.usecases.GetWatchHistoryUseCase
 import com.elhady.movies.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,7 +20,20 @@ class WatchHistoryViewModel @Inject constructor(
     val uiState = _uiState.asStateFlow()
 
     override fun getData() {
-        getWatchHistoryUseCase()
+        viewModelScope.launch {
+            val result = getWatchHistoryUseCase().collect{ list ->
+                list.map {
+
+                }
+
+            }
+
+           _uiState.update {
+                it.copy(allMedia = result)
+            }
+        }
+
+
     }
 
 
