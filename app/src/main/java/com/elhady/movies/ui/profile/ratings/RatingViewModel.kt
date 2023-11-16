ae726be658f1d3f82c7cec0e13636663673eb78c
elhady <i.elhady44@gmail.com>
2023-11-16 06:16:17 +0200

    add rated ui state mapper

--- app/src/main/java/com/elhady/movies/ui/profile/ratings/RatingViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/profile/ratings/RatingViewModel.kt
@@ -1,13 +1,28 @@
 package com.elhady.movies.ui.profile.ratings
 
+import androidx.lifecycle.viewModelScope
+import com.elhady.movies.domain.usecases.GetListOfRatedUseCase
 import com.elhady.movies.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RatingViewModel @Inject constructor() : BaseViewModel() {
+class RatingViewModel @Inject constructor(private val getListOfRatedUseCase: GetListOfRatedUseCase) : BaseViewModel() {
+
+    private val _rateUiState = MutableStateFlow(MyRateUiState())
+    val rateUiState = _rateUiState.asStateFlow()
 
     override fun getData() {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            val result = getListOfRatedUseCase()
+            _rateUiState.update {
+                it.copy(ratedList = result)
+            }
+
+        }
     }
 }
