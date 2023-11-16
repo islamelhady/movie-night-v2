a229497f0318a1f15471c0480aec3181f537ef49
elhady <i.elhady44@gmail.com>
2023-11-16 05:01:21 +0200

    add rating adapter to series details adapter

--- app/src/main/java/com/elhady/movies/ui/seriesDetails/SeriesItems.kt
+++ app/src/main/java/com/elhady/movies/ui/seriesDetails/SeriesItems.kt
@@ -1,5 +1,6 @@
 package com.elhady.movies.ui.seriesDetails
 
+import androidx.lifecycle.ViewModel
 import com.elhady.movies.ui.models.ActorUiState
 import com.elhady.movies.ui.models.MediaUiState
 import com.elhady.movies.ui.models.ReviewUiState
@@ -10,7 +11,8 @@ sealed class SeriesItems(val priority: Int){
     data class Cast(val data: List<ActorUiState>): SeriesItems(1)
     data class Similar(val data: List<MediaUiState>): SeriesItems(2)
     data class Season(val data: List<SeasonUiState>): SeriesItems(3)
-    object ReviewText: SeriesItems(4)
-    data class Review(val data: ReviewUiState): SeriesItems(5)
-    object SeeAllReviews: SeriesItems(6)
+    data class Rating(val viewModel: ViewModel): SeriesItems(4)
+    object ReviewText: SeriesItems(5)
+    data class Review(val data: ReviewUiState): SeriesItems(6)
+    object SeeAllReviews: SeriesItems(7)
 }
