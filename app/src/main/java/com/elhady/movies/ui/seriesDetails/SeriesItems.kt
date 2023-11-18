75d2e47b66adffaebd7e13d8a51b02ab819977ac
elhady <i.elhady44@gmail.com>
2023-11-18 07:20:17 +0200

    change position of rate series

--- app/src/main/java/com/elhady/movies/ui/seriesDetails/SeriesItems.kt
+++ app/src/main/java/com/elhady/movies/ui/seriesDetails/SeriesItems.kt
@@ -8,10 +8,10 @@ import com.elhady.movies.ui.seriesDetails.seriesUiState.SeasonUiState
 
 sealed class SeriesItems(val priority: Int){
     data class Header(val data: SeriesDetailsResultUiState): SeriesItems(0)
-    data class Cast(val data: List<ActorUiState>): SeriesItems(1)
-    data class Similar(val data: List<MediaUiState>): SeriesItems(2)
-    data class Season(val data: List<SeasonUiState>): SeriesItems(3)
-    data class Rating(val viewModel: ViewModel): SeriesItems(4)
+    data class Rating(val viewModel: ViewModel): SeriesItems(1)
+    data class Cast(val data: List<ActorUiState>): SeriesItems(2)
+    data class Similar(val data: List<MediaUiState>): SeriesItems(3)
+    data class Season(val data: List<SeasonUiState>): SeriesItems(4)
     object ReviewText: SeriesItems(5)
     data class Review(val data: ReviewUiState): SeriesItems(6)
     object SeeAllReviews: SeriesItems(7)
