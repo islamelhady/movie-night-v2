be61938ed285f2bb9666614187562dc9fe761e06
elhady <i.elhady44@gmail.com>
2023-11-18 10:07:21 +0200

    add view model create list use case

--- app/src/main/java/com/elhady/movies/domain/usecases/favList/CreateListUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/favList/CreateListUseCase.kt
@@ -8,10 +8,15 @@ class CreateListUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
     private val accountRepository: AccountRepository
 ) {
-    suspend operator fun invoke(listName: String) {
+    suspend operator fun invoke(listName: String): Boolean {
         val sessionId = accountRepository.getSessionId()
-        sessionId?.let {
-            movieRepository.createList(sessionId = it, name = listName)
-        }
+        return sessionId?.let {
+            val item = movieRepository.createList(sessionId = it, name = listName)
+            if (item?.success == true){
+                throw Throwable("Success")
+            }else{
+                throw Throwable("failed")
+            }
+        }?: throw Throwable("not success")
     }
 }
--- app/src/main/java/com/elhady/movies/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/favorite/FavoriteViewModel.kt
@@ -1,8 +1,13 @@
 package com.elhady.movies.ui.favorite
 
+import androidx.lifecycle.viewModelScope
 import com.elhady.movies.domain.usecases.favList.CreateListUseCase
 import com.elhady.movies.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,7 +15,16 @@ class FavoriteViewModel @Inject  constructor(
     private val createListUseCase: CreateListUseCase
 ) : BaseViewModel() {
 
+    private val _uiState = MutableStateFlow(CreateListDialogUiState())
+    val uiState = _uiState.asStateFlow()
 
     override fun getData() {
+        viewModelScope.launch {
+            createListUseCase(_uiState.value.listName)
+        }
+    }
+
+    fun onClickCreateList(listName: CharSequence){
+        _uiState.update { it.copy(listName = listName.toString()) }
     }
 }
