1bbd775994ef7ea6214be2c34ea9a0e98aabfbcc
elhady <i.elhady44@gmail.com>
2023-11-18 07:33:10 +0200

    change position of rating movie

--- app/src/main/java/com/elhady/movies/ui/movieDetails/DetailsItem.kt
+++ app/src/main/java/com/elhady/movies/ui/movieDetails/DetailsItem.kt
@@ -8,10 +8,10 @@ import com.elhady.movies.ui.models.ReviewUiState
 
 sealed class DetailsItem(val priority: Int){
     data class Header(val data: MovieDetailsUiState): DetailsItem(0)
-    data class Cast(val data: List<ActorUiState>): DetailsItem(1)
-    data class Similar(val data: List<MediaUiState>): DetailsItem(2)
-    object ReviewsText: DetailsItem(3)
-    data class Rating(val viewModel: ViewModel): DetailsItem(4)
+    data class Rating(val viewModel: ViewModel): DetailsItem(1)
+    data class Cast(val data: List<ActorUiState>): DetailsItem(2)
+    data class Similar(val data: List<MediaUiState>): DetailsItem(3)
+    object ReviewsText: DetailsItem(4)
     data class Reviews(val data: ReviewUiState): DetailsItem(5)
-    object SeeAllReviewsButton: DetailsItem(8)
+    object SeeAllReviewsButton: DetailsItem(6)
 }
