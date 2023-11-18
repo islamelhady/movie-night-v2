db95fba655c4e454d5b3a2dfd0674ec1cb0ea0a4
elhady <i.elhady44@gmail.com>
2023-11-18 07:52:43 +0200

    rename logout dialog layout

--- app/src/main/java/com/elhady/movies/ui/profile/logout/LogoutDialog.kt
+++ app/src/main/java/com/elhady/movies/ui/profile/logout/LogoutDialog.kt
@@ -7,16 +7,16 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.elhady.movies.R
-import com.elhady.movies.databinding.FragmentLogoutDialogBinding
+import com.elhady.movies.databinding.LogoutDialogBinding
 import com.elhady.movies.ui.base.BaseDialog
 import com.elhady.movies.utilities.collectLast
 import com.elhady.movies.utilities.setWidthPercent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LogoutDialog : BaseDialog<FragmentLogoutDialogBinding>() {
+class LogoutDialog : BaseDialog<LogoutDialogBinding>() {
 
-    override val layoutIdFragment: Int = R.layout.fragment_logout_dialog
+    override val layoutIdFragment: Int = R.layout.logout_dialog
     override val viewModel: LogoutDialogViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
