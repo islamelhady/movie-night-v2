c68f205f05207a506292aa9607d83e6dbf389c79
elhady <i.elhady44@gmail.com>
2023-11-18 12:57:28 +0200

    update create list use case

--- app/src/main/java/com/elhady/movies/domain/usecases/favList/CreateListUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/favList/CreateListUseCase.kt
@@ -2,21 +2,23 @@ package com.elhady.movies.domain.usecases.favList
 
 import com.elhady.movies.data.repository.AccountRepository
 import com.elhady.movies.data.repository.MovieRepository
+import com.elhady.movies.domain.models.CreatedList
 import javax.inject.Inject
 
 class CreateListUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
-    private val accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
+    private val getCreatedListUseCase: GetCreatedListUseCase
 ) {
-    suspend operator fun invoke(listName: String): Boolean {
+    suspend operator fun invoke(listName: String): List<CreatedList> {
         val sessionId = accountRepository.getSessionId()
         return sessionId?.let {
             val item = movieRepository.createList(sessionId = it, name = listName)
-            if (item?.success == true){
-                throw Throwable("Success")
-            }else{
+            if (item?.success == true) {
+                getCreatedListUseCase()
+            } else {
                 throw Throwable("failed")
             }
-        }?: throw Throwable("not success")
+        } ?: throw Throwable("not success")
     }
 }
