430521cebaddc3acf0194113dbae99d473cde5b7
elhady <i.elhady44@gmail.com>
2023-11-19 14:45:11 +0200

    add movie to list

--- app/src/main/java/com/elhady/movies/domain/usecases/favList/SaveMovieToFavListUseCase.kt
+++ app/src/main/java/com/elhady/movies/domain/usecases/favList/SaveMovieToFavListUseCase.kt
@@ -1,21 +1,30 @@
 package com.elhady.movies.domain.usecases.favList
 
+import com.elhady.movies.data.repository.AccountRepository
 import com.elhady.movies.data.repository.MovieRepository
 import com.elhady.movies.utilities.checkIfExist
 import javax.inject.Inject
 
 class SaveMovieToFavListUseCase @Inject constructor(
+    private val accountRepository: AccountRepository,
     private val movieRepository: MovieRepository
 ) {
 
-    suspend operator fun invoke(listID: Int, mediaId: Int): String {
-        val result = movieRepository.getListDetails(listID)
+    suspend operator fun invoke(listId: Int, mediaId: Int): String {
+        val result = movieRepository.getListDetails(listId)
 
         return if (result?.checkIfExist(mediaId) == true) {
             "Fail: this movie is already on the list"
         } else {
-            TODO()
+            addMovieToList(listId, mediaId)
         }
     }
 
+    private suspend fun addMovieToList(listId: Int, mediaId: Int): String {
+        val sessionId = accountRepository.getSessionId()
+        return sessionId?.let {
+            movieRepository.addMovieToList(it, listId, mediaId)
+            "Success: The movie has been added"
+        } ?: throw Throwable("no login")
+    }
 }
