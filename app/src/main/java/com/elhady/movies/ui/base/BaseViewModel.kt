65561a232c40328b1881fd82b1ae02804058fc88
elhady <i.elhady44@gmail.com>
2023-11-21 10:04:28 +0200

    add try to execute fun wrapper

--- app/src/main/java/com/elhady/movies/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/base/BaseViewModel.kt
@@ -1,8 +1,30 @@
 package com.elhady.movies.ui.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
 
 abstract class BaseViewModel : ViewModel(){
 
     abstract fun getData()
+
+    protected fun <T> tryToExecute(
+        call: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (Throwable) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ){
+        viewModelScope.launch(dispatcher) {
+            try {
+                call().also(onSuccess)
+            } catch (th: Throwable){
+                onError(th)
+            }
+        }
+    }
+
+
 }
