828f7386b2395d2b24e19088fb7714fbb9b95845
elhady <i.elhady44@gmail.com>
2023-11-21 06:30:13 +0200

    DELETE base mapper and update mapper

--- app/src/main/java/com/elhady/movies/domain/mappers/BaseMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.elhady.movies.domain.mappers
-
-interface BaseMapper<INPUT, OUTPUT>{
-    fun map(input: INPUT): OUTPUT
-    fun map(input: List<INPUT>): List<OUTPUT>{
-        return input.map(::map)
-    }
-}
