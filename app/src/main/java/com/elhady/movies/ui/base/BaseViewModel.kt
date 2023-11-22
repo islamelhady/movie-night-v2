b5063894fa28e3f7aeb1c2732af72b9e46d3f1fb
elhady <i.elhady44@gmail.com>
2023-11-22 10:33:26 +0200

    update base view model and add event as a shared flow

--- app/src/main/java/com/elhady/movies/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/base/BaseViewModel.kt
@@ -5,16 +5,21 @@ import androidx.lifecycle.viewModelScope
 import com.elhady.movies.domain.mappers.Mapper
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<STATE>(initialState: STATE) : ViewModel() {
+abstract class BaseViewModel<STATE, EVENT>(initialState: STATE) : ViewModel() {
 
     protected val _state : MutableStateFlow<STATE> by lazy {
         MutableStateFlow(initialState)
     }
     val state = _state.asStateFlow()
+
+    protected val _event = MutableSharedFlow<EVENT>()
+    val event = _event.asSharedFlow()
     abstract fun getData()
 
     protected fun <T> tryToExecute(
@@ -48,5 +53,10 @@ abstract class BaseViewModel<STATE>(initialState: STATE) : ViewModel() {
         }
     }
 
+    protected fun sendEvent(event: EVENT){
+        viewModelScope.launch {
+            _event.emit(event)
+        }
+    }
 
 }
