5e50fdda37dac5ff4492749f2bc284a1f9eca4c5
elhady <i.elhady44@gmail.com>
2023-12-23 10:32:40 +0200

    add handel no connection

--- app/src/main/java/com/elhady/movies/ui/actors/ActorsViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/actors/ActorsViewModel.kt
@@ -3,15 +3,20 @@ package com.elhady.movies.ui.actors
 import androidx.lifecycle.viewModelScope
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
+import androidx.paging.Pager
+import androidx.paging.PagingData
 import androidx.paging.map
 import com.elhady.movies.domain.usecases.GetAllActorsUseCase
 import com.elhady.movies.ui.base.BaseViewModel
 import com.elhady.movies.ui.home.adapters.ActorInteractionListener
 import com.elhady.movies.ui.mappers.ActorUiMapper
+import com.elhady.movies.ui.models.ActorUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,45 +30,55 @@ class ActorsViewModel @Inject constructor(
     }
 
     override fun getData() {
-        _state.update { it.copy(isLoading = true) }
+        _state.update { it.copy(isLoading = true, onErrors = emptyList()) }
         getAllActors()
     }
 
 
     private fun getAllActors() {
-        viewModelScope.launch {
-            val items = getAllActorsUseCase().map { paging ->
-                paging.map {
-                    actorUiMapper.map(it)
+        try {
+            viewModelScope.launch {
+                val items = getAllActorsUseCase().map { paging ->
+                    paging.map {
+                        actorUiMapper.map(it)
+                    }
+                }
+                _state.update {
+                    it.copy(
+                        actors = items,
+                        isLoading = false,
+                        onErrors = emptyList()
+                    )
                 }
             }
-            _state.update {
-                it.copy(
-                    actors = items,
-                    isLoading = false,
-                    error = emptyList()
-                )
-            }
+        }catch (th: UnknownHostException){
+            onError(th)
         }
+    }
+
+    private fun onError(th: Throwable){
+        val errors = _state.value.onErrors.toMutableList()
+        errors.add(th.message.toString())
+        _state.update { it.copy(onErrors = errors, isLoading = false) }
     }
 
     fun setErrorUiState(combinedLoadStates: CombinedLoadStates) {
         when (combinedLoadStates.refresh) {
             is LoadState.NotLoading -> {
                 _state.update {
-                    it.copy(isLoading = false, error = emptyList())
+                    it.copy(isLoading = false, onErrors = emptyList())
                 }
             }
 
             is LoadState.Loading -> {
                 _state.update {
-                    it.copy(isLoading = true, error = emptyList())
+                    it.copy(isLoading = true, onErrors = emptyList())
                 }
             }
 
             is LoadState.Error -> {
                 _state.update {
-                    it.copy(isLoading = false, error = listOf(Error("")))
+                    it.copy(isLoading = false, onErrors = listOf(("No Network Connection")))
                 }
             }
         }
