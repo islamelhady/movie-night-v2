41363aa94533115cdbfca1906b8f00c555ec76a0
elhady <i.elhady44@gmail.com>
2024-01-10 00:09:13 +0200

    add switch theme

--- app/src/main/java/com/elhady/movies/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/elhady/movies/ui/profile/ProfileFragment.kt
@@ -1,7 +1,9 @@
 package com.elhady.movies.ui.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.elhady.movies.R
@@ -18,7 +20,9 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileUiState, Pro
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setTitle(true)
+        changeAppTheme()
     }
+
     override fun onEvent(event: ProfileUiEvent) {
         val action = when (event) {
             ProfileUiEvent.DialogLogoutEvent -> ProfileFragmentDirections.actionProfileFragmentToLogoutDialog()
@@ -27,6 +31,26 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, ProfileUiState, Pro
             ProfileUiEvent.LoginEvent -> ProfileFragmentDirections.actionProfileFragmentToLoginFragment(2)
         }
         findNavController().navigate(action)
+    }
+
+    private fun changeAppTheme() {
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val switchButtonTheme = binding.switchBottonTheme
+        val savedThemeState = sharedPreferences.getBoolean(PREF_THEME_STATE, false)
+        switchButtonTheme.isChecked = savedThemeState
+
+        switchButtonTheme.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean(PREF_THEME_STATE, isChecked).apply()
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    companion object{
+        private const val PREF_THEME_STATE = "theme"
     }
 
 
