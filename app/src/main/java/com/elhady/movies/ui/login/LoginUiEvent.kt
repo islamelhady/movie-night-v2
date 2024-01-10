a61ed5b2f852cf46a49ccbe51627224568b688d7
elhady <i.elhady44@gmail.com>
2024-01-10 22:34:51 +0200

    handel on failed login

--- app/src/main/java/com/elhady/movies/ui/login/LoginUiEvent.kt
+++ app/src/main/java/com/elhady/movies/ui/login/LoginUiEvent.kt
@@ -3,4 +3,5 @@ package com.elhady.movies.ui.login
 sealed interface LoginUiEvent {
     data class LoginEvent(val login: Int) : LoginUiEvent
     object SignUpEvent : LoginUiEvent
+    data class ShowSnackBar(val message: String): LoginUiEvent
 }
