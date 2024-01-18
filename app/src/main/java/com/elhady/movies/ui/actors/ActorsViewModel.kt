71cddc3e68cdcad913f1eb62a957e9c2cf9a85ca
elhady <i.elhady44@gmail.com>
2024-01-18 07:53:41 +0200

    enhance actor viewmodel

--- app/src/main/java/com/elhady/movies/ui/actors/ActorsViewModel.kt
+++ app/src/main/java/com/elhady/movies/ui/actors/ActorsViewModel.kt
@@ -1,22 +1,18 @@
 package com.elhady.movies.ui.actors
 
-import androidx.lifecycle.viewModelScope
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
-import androidx.paging.Pager
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.elhady.movies.domain.models.Actor
 import com.elhady.movies.domain.usecases.GetAllActorsUseCase
 import com.elhady.movies.ui.base.BaseViewModel
 import com.elhady.movies.ui.home.adapters.ActorInteractionListener
 import com.elhady.movies.ui.mappers.ActorUiMapper
-import com.elhady.movies.ui.models.ActorUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import java.net.UnknownHostException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -31,30 +27,20 @@ class ActorsViewModel @Inject constructor(
 
     override fun getData() {
         _state.update { it.copy(isLoading = true, onErrors = emptyList()) }
-        getAllActors()
+        tryToExecute(
+            call = { getAllActorsUseCase()},
+            onSuccess = ::onSuccessActors,
+            onError = ::onError
+        )
     }
 
-
-    private fun getAllActors() {
-        try {
-            viewModelScope.launch {
-                val items = getAllActorsUseCase().map { paging ->
-                    paging.map {
-                        actorUiMapper.map(it)
-                    }
-                }
-                _state.update {
-                    it.copy(
-                        actors = items,
-                        isLoading = false,
-                        onErrors = emptyList()
-                    )
-                }
-            }
-        }catch (th: UnknownHostException){
-            onError(th)
+    private fun onSuccessActors(actors: Flow<PagingData<Actor>>){
+        val result = actors.map {  pagingData->
+            pagingData.map(actorUiMapper::map)
         }
+        _state.update { it.copy(isLoading = false, actors = result, onErrors = emptyList()) }
     }
+
 
     private fun onError(th: Throwable){
         val errors = _state.value.onErrors.toMutableList()
