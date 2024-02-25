4264f428456e1b478b2e97a69aef59da84d73279
islam elhady <i.elhady44@gmail.com>
2024-02-26 00:43:52 +0200

    add retrofit dependencies of remote module

--- core/data/remote/build.gradle.kts
+++ core/data/remote/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -40,4 +41,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    // okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
 }
