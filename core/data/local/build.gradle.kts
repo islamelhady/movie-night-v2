108f98d965d5c430b049322f7547b7ca7e2c4a9a
islam elhady <i.elhady44@gmail.com>
2024-02-28 01:05:33 +0200

    add implementation of remote module

--- core/data/local/build.gradle.kts
+++ core/data/local/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:data:remote"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
