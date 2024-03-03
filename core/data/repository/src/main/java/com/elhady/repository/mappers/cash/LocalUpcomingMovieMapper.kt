c4cf5d0e07c32b348b65ff8dccdd545438988761
islam elhady <i.elhady44@gmail.com>
2024-03-03 19:43:01 +0200

    refactor upcoming movie

--- core/data/repository/src/main/java/com/elhady/repository/mappers/cash/LocalUpcomingMovieMapper.kt
+++ core/data/repository/src/main/java/com/elhady/repository/mappers/cash/LocalUpcomingMovieMapper.kt
@@ -1,17 +1,18 @@
-package com.elhady.local.mappers.movies
+package com.elhady.repository.mappers.cash
 
 import com.elhady.local.Constants
 import com.elhady.local.database.dto.movies.UpcomingMovieLocalDto
-import com.elhady.local.mappers.Mapper
+import com.elhady.movies.domain.mappers.Mapper
 import com.elhady.remote.response.dto.MovieRemoteDto
 import javax.inject.Inject
 
-class UpcomingMovieMapper @Inject constructor(): Mapper<MovieRemoteDto, UpcomingMovieLocalDto> {
+class LocalUpcomingMovieMapper @Inject constructor() :
+    Mapper<MovieRemoteDto, UpcomingMovieLocalDto> {
     override fun map(input: MovieRemoteDto): UpcomingMovieLocalDto {
         return UpcomingMovieLocalDto(
             id = input.id ?: 0,
             title = input.title ?: "",
-            imageUrl = (Constants.IMAGE_PATH + input.posterPath)
+            imageUrl = Constants.IMAGE_PATH + input.posterPath
         )
     }
 }
--- core/data/repository/src/main/java/com/elhady/repository/mediaDataSource/movies/UpcomingMovieDataSource.kt
+++ core/data/repository/src/main/java/com/elhady/repository/mediaDataSource/movies/UpcomingMovieDataSource.kt
@@ -1,20 +1,20 @@
 package com.elhady.repository.mediaDataSource.movies
 
-import com.elhady.remote.response.dto.MovieDto
+import com.elhady.remote.response.dto.MovieRemoteDto
 import com.elhady.remote.serviece.MovieService
 import com.elhady.repository.mediaDataSource.BasePagingSource
 import javax.inject.Inject
 
-class UpcomingMovieDataSource @Inject constructor(private val service: MovieService) : BasePagingSource<MovieDto>() {
+class UpcomingMovieDataSource @Inject constructor(private val service: MovieService) : BasePagingSource<MovieRemoteDto>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieDto> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieRemoteDto> {
         val pageNumber = params.key ?: 1
 
         return try {
             val response = service.getUpcomingMovies(page = pageNumber)
 
             LoadResult.Page(
-                data = response.body()?.items ?: emptyList(),
+                data = response.body()?.result ?: emptyList(),
                 prevKey = null,
                 nextKey = response.body()?.page?.plus(1)
             )
