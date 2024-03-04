888e7b2302d2443b6039071f122638afbcd6f093
islam elhady <i.elhady44@gmail.com>
2024-03-05 01:12:14 +0200

    add home ui state

--- feature/home/presentation/viewModel/src/main/java/com/elhady/viewmodel/home/homeUiState/HomeUiState.kt
+++ feature/home/presentation/viewModel/src/main/java/com/elhady/viewmodel/home/homeUiState/HomeUiState.kt
@@ -1,22 +1,115 @@
 package com.elhady.viewmodel.home.homeUiState
 
-import com.elhady.viewmodel.models.ActorUiState
-import com.elhady.viewmodel.models.MediaUiState
-import com.elhady.viewmodel.models.PopularUiState
+import com.elhady.usecase.seeAllMedia.ShowMoreType
+import kotlin.math.roundToInt
 
 data class HomeUiState(
-    val popularMovieSlider: List<PopularUiState> = emptyList(),
-    val upcomingMovie: List<MediaUiState> = emptyList(),
-    val trendingMovie: List<MediaUiState> = emptyList(),
-    val nowPlayingMovie: List<MediaUiState> = emptyList(),
-    val topRatedMovie: List<MediaUiState> = emptyList(),
-    val onTheAirSeries: List<MediaUiState> = emptyList(),
-    val airingTodaySeries: List<MediaUiState> = emptyList(),
-    val tvSeriesLists: List<MediaUiState> = emptyList(),
-    val mysteryMovies: List<MediaUiState> = emptyList(),
-    val adventureMovies: List<MediaUiState> = emptyList(),
-    val popularPeople: List<ActorUiState> = emptyList(),
-    val type: SeeAllType = SeeAllType.POPULAR_TV,
+    val popularMovieSlider: List<PopularMoviesUiState> = emptyList(),
+    val upcomingMovie: List<UpcomingMoviesUiState> = emptyList(),
+    val trendingMovie: List<TrendingMoviesUiState> = emptyList(),
+    val nowPlayingMovie: List<NowPlayingMoviesUiState> = emptyList(),
+    val topRatedMovie: List<TopRatedMoviesUiState> = emptyList(),
+    val onTheAirTVShows: List<OnTheAirTVShowsUiState> = emptyList(),
+    val airingTodayTVShows: List<AiringTodayTVShowsUiState> = emptyList(),
+    val tvShowsLists: List<TVShowsListsUiState> = emptyList(),
+    val mysteryMovies: List<MysteryMoviesUiState> = emptyList(),
+    val adventureMovies: List<AdventureMoviesUiState> = emptyList(),
+    val popularPeople: List<PopularPeopleUiState> = emptyList(),
+    val type: ShowMoreType = ShowMoreType.POPULAR_TV,
     val isLoading: Boolean = false,
     val onErrors: List<String> = emptyList()
+)
+
+data class PopularMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+    val genre: List<String>
+
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class UpcomingMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val title: String,
+)
+
+data class TrendingMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class NowPlayingMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val title: String,
+    val rate: Double
+){
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class TopRatedMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class OnTheAirTVShowsUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class AiringTodayTVShowsUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class TVShowsListsUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class MysteryMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class AdventureMoviesUiState(
+    val id: Int,
+    val imageUrl: String,
+    val rate: Double,
+    val title: String,
+) {
+    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
+}
+
+data class PopularPeopleUiState(
+    val id: Int,
+    val profilePath: String,
+    val name: String
 )
