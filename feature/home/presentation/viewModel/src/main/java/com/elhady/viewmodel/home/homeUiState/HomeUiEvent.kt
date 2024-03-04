8024f786d18506948e1690ae2afdf8482d70f69b
islam elhady <i.elhady44@gmail.com>
2024-03-05 01:17:04 +0200

    refactor home ui event

--- feature/home/presentation/viewModel/src/main/java/com/elhady/viewmodel/home/homeUiState/HomeUiEvent.kt
+++ feature/home/presentation/viewModel/src/main/java/com/elhady/viewmodel/home/homeUiState/HomeUiEvent.kt
@@ -1,11 +1,13 @@
 package com.elhady.viewmodel.home.homeUiState
 
+import com.elhady.usecase.seeAllMedia.ShowMoreType
+
 
 sealed interface HomeUiEvent {
-    data class ClickMovieEvent(val movieID: Int) : HomeUiEvent
-    data class ClickSeeAllMoviesEvent(val mediaType: SeeAllType) : HomeUiEvent
-    object ClickSeeAllActorsEvent : HomeUiEvent
-    data class ClickActorEvent(val actorID: Int): HomeUiEvent
-    data class ClickSeeAllSeriesEvent(val mediaType: SeeAllType): HomeUiEvent
-    data class ClickSeriesEvent(val mediaID: Int): HomeUiEvent
+    data class ClickMovieEvent(val id: Int) : HomeUiEvent
+    data class ClickShowMoreMoviesEvent(val mediaType: ShowMoreType) : HomeUiEvent
+    data class ClickShowMoreActorsEvent(val mediaType: ShowMoreType) : HomeUiEvent
+    data class ClickActorEvent(val id: Int): HomeUiEvent
+    data class ClickShowMoreTVShowEvent(val mediaType: ShowMoreType): HomeUiEvent
+    data class ClickTVShowEvent(val id: Int): HomeUiEvent
 }
