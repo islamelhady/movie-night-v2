f7c8e8dbd906a5f1de6abe6549cdc914073abff3
islam elhady <i.elhady44@gmail.com>
2024-03-07 02:04:03 +0200

    add wrapper pager higher order function

--- core/base/src/main/java/com/elhady/base/BaseViewModel.kt
+++ core/base/src/main/java/com/elhady/base/BaseViewModel.kt
@@ -2,25 +2,28 @@ package com.elhady.base
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.elhady.mapper.Mapper
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<STATE, EVENT>(initialState: STATE) : ViewModel() {
 
-    protected val _state : MutableStateFlow<STATE> by lazy {
+    protected val _state: MutableStateFlow<STATE> by lazy {
         MutableStateFlow(initialState)
     }
     val state = _state.asStateFlow()
 
     private val _event = MutableSharedFlow<EVENT>()
     val event = _event.asSharedFlow()
-    abstract fun getData()
 
     protected fun <T> tryToExecute(
         call: suspend () -> T,
@@ -53,6 +56,7 @@ abstract class BaseViewModel<STATE, EVENT>(initialState: STATE) : ViewModel() {
         }
     }
 
+
     protected fun <INPUT, OUTPUT> tryToExecute(
         call: suspend () -> INPUT,
         mapper: Mapper<INPUT, OUTPUT>,
@@ -69,7 +73,27 @@ abstract class BaseViewModel<STATE, EVENT>(initialState: STATE) : ViewModel() {
         }
     }
 
-    protected fun sendEvent(event: EVENT){
+    protected fun <INPUT : Any, OUTPUT : Any> wrapperPager(
+        data: suspend () -> Flow<PagingData<INPUT>>,
+        mapper: (INPUT) -> OUTPUT,
+        onSuccess: (Flow<PagingData<OUTPUT>>) -> Unit,
+        onError: (Throwable) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ) {
+        viewModelScope.launch(dispatcher) {
+            try {
+                data().map { pagingData ->
+                    pagingData.map {
+                        mapper(it)
+                    }
+                }.also(onSuccess)
+            } catch (th: Throwable) {
+                onError(th)
+            }
+        }
+    }
+
+    protected fun sendEvent(event: EVENT) {
         viewModelScope.launch {
             _event.emit(event)
         }
