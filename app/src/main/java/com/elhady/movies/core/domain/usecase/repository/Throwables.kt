0abf86ab38e93b7eb30856daaa49101dc3857135
islam elhady <i.elhady44@gmail.com>
2024-03-23 19:19:07 +0200

    update throwable

--- app/src/main/java/com/elhady/movies/core/domain/usecase/repository/Throwables.kt
+++ app/src/main/java/com/elhady/movies/core/domain/usecase/repository/Throwables.kt
@@ -2,8 +2,8 @@ package com.elhady.movies.core.domain.usecase.repository
 
 open class ApiThrowable(message: String?): Throwable(message)
 class UnauthorizedThrowable: ApiThrowable("Unauthorized")
-class NoNetworkThrowable: ApiThrowable("No Network")
-class TimeoutThrowable: ApiThrowable("Not Logged In")
+class NoNetworkThrowable: ApiThrowable("noNetwork")
+class TimeoutThrowable: ApiThrowable("timeout")
 class ParsingThrowable: ApiThrowable("Parsing Error")
 class ServerErrorThrowable: ApiThrowable("Server Error")
-
+class ForbiddenThrowable: ApiThrowable("Refuses to authorize it")
