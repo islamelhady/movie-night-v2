d8517a866b1e2fb289758895f7c667bdecce2d42
islam elhady <i.elhady44@gmail.com>
2024-03-23 15:54:57 +0200

    add common use case

---
+++ app/src/main/java/com/elhady/movies/core/domain/usecase/usecase/common/CreateUserListUseCase.kt
@@ -0,0 +1,13 @@
+package com.elhady.movies.core.domain.usecase.usecase.common
+
+import com.elhady.movies.core.domain.entities.StatusEntity
+import com.elhady.movies.core.domain.usecase.repository.MovieRepository
+import javax.inject.Inject
+
+class CreateUserListUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(listName: String): StatusEntity {
+        return movieRepository.createUserList(listName)
+    }
+}
