39951023a3c79ad8ecfd341f78f72ac053979913
islam elhady <i.elhady44@gmail.com>
2024-03-23 19:34:16 +0200

    refactor review ent

--- app/src/main/java/com/elhady/movies/core/domain/entities/ReviewEntity.kt
+++ app/src/main/java/com/elhady/movies/core/domain/entities/ReviewEntity.kt
@@ -1,13 +1,6 @@
 package com.elhady.movies.core.domain.entities
 
 data class ReviewEntity(
-    val name:String,
-    val avatar_path:String,
-    val content:String,
-    val created_at:String,
-)
-
-data class ReviewEntity(
     val name: String = "",
     val avatarPath: String = "",
     val content: String = "",
