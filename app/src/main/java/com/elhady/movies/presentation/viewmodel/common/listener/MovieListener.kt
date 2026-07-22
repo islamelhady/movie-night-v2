2d8eb77a28c16e1accc5437bc3036fe8cbb27da4
islam elhady <i.elhady44@gmail.com>
2026-07-22 06:27:57 +0300

    remove search feature in app module

--- app/src/main/java/com/elhady/movies/presentation/viewmodel/common/listener/MovieListener.kt
+++
@@ -1,8 +0,0 @@
-package com.elhady.movies.presentation.viewmodel.common.listener
-
-import com.elhady.movies.core.common.bases.BaseInteractionListener
-
-
-interface MovieListener: BaseInteractionListener {
-    fun onClickMedia(id: Int)
-}
