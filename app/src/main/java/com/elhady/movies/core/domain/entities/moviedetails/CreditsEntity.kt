26c5b9d162843e1a4f9242408055573fbf869931
islam elhady <i.elhady44@gmail.com>
2026-07-22 05:10:20 +0300

    delete home screen and domain entities

--- app/src/main/java/com/elhady/movies/core/domain/entities/moviedetails/CreditsEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.elhady.movies.core.common.domain.entities.moviedetails
-
-data class CreditsEntity(
-    val cast: List<CastEntity> = emptyList(),
-    val crew: List<CrewEntity> =emptyList()
-)
