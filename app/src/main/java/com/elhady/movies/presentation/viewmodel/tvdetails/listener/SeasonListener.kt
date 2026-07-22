5cd9868a6ce3c536ba7a66d25a3ecc0aa87b69cc
islam elhady <i.elhady44@gmail.com>
2026-07-22 18:34:46 +0300

    remove details screens and presentation logic from app module

--- app/src/main/java/com/elhady/movies/presentation/viewmodel/tvdetails/listener/SeasonListener.kt
+++
@@ -1,8 +0,0 @@
-package com.elhady.movies.presentation.viewmodel.tvdetails.listener
-
-import com.elhady.movies.core.common.bases.BaseInteractionListener
-
-
-interface SeasonListener : BaseInteractionListener {
-    fun onClickSeason(seasonNumber: Int)
-}
--- app/src/main/java/com/elhady/movies/presentation/viewmodel/tvdetails/listener/ShowMoreRecommended.kt
+++
@@ -1,5 +0,0 @@
-package com.elhady.movies.presentation.viewmodel.tvdetails.listener
-
-interface ShowMoreRecommended {
-    fun onShowMoreRecommended()
-}
