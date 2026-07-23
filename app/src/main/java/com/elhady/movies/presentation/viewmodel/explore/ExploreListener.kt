3f31e962272dafa5034172ebf9c5b31ef9a18dc7
islam elhady <i.elhady44@gmail.com>
2026-07-23 04:07:30 +0300

    delete explore viewmodel

--- app/src/main/java/com/elhady/movies/presentation/viewmodel/explore/ExploreListener.kt
+++
@@ -1,7 +0,0 @@
-package com.elhady.movies.presentation.viewmodel.explore
-
-import com.elhady.movies.core.common.presentation.MovieListener
-
-interface ExploreListener: MovieListener {
-    fun onClickSearch()
-}
