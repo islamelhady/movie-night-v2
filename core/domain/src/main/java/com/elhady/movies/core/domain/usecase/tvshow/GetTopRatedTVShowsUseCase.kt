d7ae04f5b64390ff6f1ca134b1591f740df30171
islam elhady <i.elhady44@gmail.com>
2026-07-24 03:46:11 +0300

    move TV Show-specific fragments, adapters, and viewmodels

---
+++ core/domain/src/main/java/com/elhady/movies/core/domain/usecase/tvshow/GetTopRatedTVShowsUseCase.kt
@@ -0,0 +1,15 @@
+package com.elhady.movies.core.domain.usecase.tvshow
+
+import androidx.paging.PagingData
+import com.elhady.movies.core.domain.model.TVShowsEntity
+import com.elhady.movies.core.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetTopRatedTVShowsUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(): Flow<PagingData<TVShowsEntity>> {
+        return movieRepository.getTopRatedTVShowsPager().flow
+    }
+}
