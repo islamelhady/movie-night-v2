156e8b50cb375f5e291eb5425e01aff6b256ddd3
islam elhady <i.elhady44@gmail.com>
2026-07-27 00:12:55 +0300

    migrate core:common to kotlin jvm library and update paging dependency in core:ui

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     api(libs.lottie)
 
     // Paging (for BaseFooterAdapter)
-    implementation(libs.androidx.paging.runtime)
+    api(libs.androidx.paging.runtime)
 
     // Recycler Swipe Decorator
     implementation(libs.recyclerview.swipe.decorator)
