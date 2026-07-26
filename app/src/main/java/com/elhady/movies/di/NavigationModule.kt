6c11ab9915e5180c37902553f41afdad2c0f148f
islam elhady <i.elhady44@gmail.com>
2026-07-26 16:04:32 +0300

    provide navigator via hilt in :app

---
+++ app/src/main/java/com/elhady/movies/di/NavigationModule.kt
@@ -0,0 +1,18 @@
+package com.elhady.movies.di
+
+import com.elhady.movies.core.common.navigation.Navigator
+import com.elhady.movies.navigation.NavigatorImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class NavigationModule {
+
+    @Binds
+    @ActivityScoped
+    abstract fun bindNavigator(navigatorImpl: NavigatorImpl): Navigator
+}
