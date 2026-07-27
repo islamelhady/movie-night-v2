a59b90859584e02167d35d3a683191a2edba8c22
islam elhady <i.elhady44@gmail.com>
2026-07-28 01:53:22 +0300

    remove unused CastUiMapper and TvDetailsCreditEntity

--- core/domain/src/main/java/com/elhady/movies/core/domain/model/tvdetails/TvDetailsCreditEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.elhady.movies.core.domain.model.tvdetails
-
-data class CastEntity(
-    val id: Int,
-    val name: String,
-    val originalName: String,
-    val profileImageUrl: String,
-    val character: String
-)
