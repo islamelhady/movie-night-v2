fc01a6beec56d506869526eb7dbf002794c30a06
islam elhady <i.elhady44@gmail.com>
2026-07-28 01:52:30 +0300

    move PeopleUiMapper to feature modules and add CastUiMapper

---
+++ feature/details/src/main/java/com/elhady/movies/feature/details/presentation/moviedetails/mapper/CastUiMapper.kt
@@ -0,0 +1,17 @@
+package com.elhady.movies.feature.details.presentation.moviedetails.mapper
+
+import com.elhady.movies.core.common.mapper.Mapper
+import com.elhady.movies.core.domain.model.moviedetails.CastEntity
+import com.elhady.movies.core.ui.model.PeopleUIState
+import javax.inject.Inject
+
+class CastUiMapper @Inject constructor() :
+    Mapper<CastEntity, PeopleUIState> {
+    override fun map(input: CastEntity): PeopleUIState {
+        return PeopleUIState(
+            id = input.id,
+            name = input.name,
+            imageUrl = input.profilePath
+        )
+    }
+}
