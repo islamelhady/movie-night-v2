4f9b5f382c821a4ad40e37aa8db5abc79d983304
islam elhady <i.elhady44@gmail.com>
2026-07-28 05:13:18 +0300

    implement SearchRepositoryImpl for movie, tv, and people search with history management

---
+++ core/data/src/main/java/com/elhady/movies/core/data/repository/SearchRepositoryImpl.kt
@@ -0,0 +1,67 @@
+package com.elhady.movies.core.data.repository
+
+import com.elhady.movies.core.data.bases.BaseRepository
+import com.elhady.movies.core.data.mapper.domain.DomainPeopleRemoteMapper
+import com.elhady.movies.core.data.mapper.domain.search.DomainMovieSearchMapper
+import com.elhady.movies.core.data.mapper.domain.search.DomainTvShowSearchMapper
+import com.elhady.movies.core.database.MovieDao
+import com.elhady.movies.core.database.dto.SearchHistoryLocalDto
+import com.elhady.movies.core.domain.model.MovieEntity
+import com.elhady.movies.core.domain.model.PeopleEntity
+import com.elhady.movies.core.domain.model.TvEntity
+import com.elhady.movies.core.domain.repository.GenreRepository
+import com.elhady.movies.core.domain.repository.SearchRepository
+import com.elhady.movies.core.network.service.MovieService
+import javax.inject.Inject
+
+class SearchRepositoryImpl @Inject constructor(
+    private val movieService: MovieService,
+    private val movieDao: MovieDao,
+    private val genreRepository: GenreRepository,
+    private val domainMovieSearchMapper: DomainMovieSearchMapper,
+    private val domainTvShowSearchMapper: DomainTvShowSearchMapper,
+    private val domainPeopleRemoteMapper: DomainPeopleRemoteMapper
+) : BaseRepository(), SearchRepository {
+
+    override suspend fun getSearchHistory(keyword: String): List<String> {
+        return movieDao.getSearchHistory("%${keyword}%").map { it.keyword }
+    }
+
+    override suspend fun searchHistory(): List<String> {
+        return movieDao.getSearchHistory().map { it.keyword }
+    }
+
+    override suspend fun insertSearchHistory(keyword: String) {
+        movieDao.insertSearchHistory(SearchHistoryLocalDto(keyword))
+    }
+
+    override suspend fun clearAllSearchHistory() {
+        movieDao.clearAllSearchHistory()
+    }
+
+    override suspend fun deleteSearchHistory(keyword: String) {
+        movieDao.deleteSearchHistory(keyword)
+    }
+
+    override suspend fun searchForMovies(keyword: String): List<MovieEntity> {
+        val movieRemoteDto =
+            wrapApiCall { movieService.searchForMovies(keyword) }.results?.filterNotNull()
+                ?: emptyList()
+        val genresEntities = genreRepository.getGenresMovies()
+        return domainMovieSearchMapper.map(movieRemoteDto, genresEntities)
+    }
+
+    override suspend fun searchForTv(keyword: String): List<TvEntity> {
+        val tvRemoteDto = wrapApiCall { movieService.searchForTv(keyword) }.results?.filterNotNull()
+            ?: emptyList()
+        val genresTvEntities = genreRepository.getGenresTvs()
+        return domainTvShowSearchMapper.map(tvRemoteDto, genresTvEntities)
+    }
+
+    override suspend fun searchForPeople(keyword: String): List<PeopleEntity> {
+        return domainPeopleRemoteMapper.map(
+            wrapApiCall { movieService.searchForPeople(keyword) }.results?.filterNotNull()
+                ?: emptyList()
+        )
+    }
+}
