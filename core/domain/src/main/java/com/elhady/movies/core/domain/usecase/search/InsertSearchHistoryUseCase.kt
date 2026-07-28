18c566d648ded3751f0a47f806e7509dbd1f520a
islam elhady <i.elhady44@gmail.com>
2026-07-28 06:21:16 +0300

    update Search Use Cases to inject SearchRepository

--- core/domain/src/main/java/com/elhady/movies/core/domain/usecase/search/InsertSearchHistoryUseCase.kt
+++ core/domain/src/main/java/com/elhady/movies/core/domain/usecase/search/InsertSearchHistoryUseCase.kt
@@ -1,12 +1,12 @@
 package com.elhady.movies.core.domain.usecase.search
 
-import com.elhady.movies.core.domain.repository.MovieRepository
+import com.elhady.movies.core.domain.repository.SearchRepository
 import javax.inject.Inject
 
 class InsertSearchHistoryUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val searchRepository: SearchRepository
 ) {
     suspend operator fun invoke(keyword: String){
-        return movieRepository.insertSearchHistory(keyword)
+        return searchRepository.insertSearchHistory(keyword)
     }
 }
