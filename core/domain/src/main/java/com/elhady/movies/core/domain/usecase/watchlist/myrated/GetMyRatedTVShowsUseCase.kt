e6f308e4c71ee78f657431134f91218bfe0a71d5
islam elhady <i.elhady44@gmail.com>
2026-07-28 06:20:13 +0300

    update tv shows use cases

--- core/domain/src/main/java/com/elhady/movies/core/domain/usecase/watchlist/myrated/GetMyRatedTVShowsUseCase.kt
+++ core/domain/src/main/java/com/elhady/movies/core/domain/usecase/watchlist/myrated/GetMyRatedTVShowsUseCase.kt
@@ -2,15 +2,15 @@ package com.elhady.movies.core.domain.usecase.watchlist.myrated
 
 import androidx.paging.PagingData
 import com.elhady.movies.core.domain.model.myrated.MyRatedTvShowEntity
-import com.elhady.movies.core.domain.repository.MovieRepository
+import com.elhady.movies.core.domain.repository.AccountRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
 class GetMyRatedTVShowsUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val accountRepository: AccountRepository
 ) {
     suspend operator fun invoke(): Flow<PagingData<MyRatedTvShowEntity>> {
-        return movieRepository.getRatedTvShows().flow
+        return accountRepository.getRatedTvShows().flow
     }
 }
