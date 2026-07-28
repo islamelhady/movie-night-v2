edda5ddf021803fcc290e89f299705f88cde2e0b
islam elhady <i.elhady44@gmail.com>
2026-07-29 00:19:07 +0300

    Add TvShowApiService to core:network module

---
+++ core/network/src/main/java/com/elhady/movies/core/network/api/TvShowApiService.kt
@@ -0,0 +1,103 @@
+package com.elhady.movies.core.network.api
+
+import com.elhady.movies.core.network.model.request.RateRequest
+import com.elhady.movies.core.network.model.request.RatingEpisodeDetailsRequest
+import com.elhady.movies.core.network.model.response.DataWrapperResponse
+import com.elhady.movies.core.network.model.response.dto.StatusResponse
+import com.elhady.movies.core.network.model.response.dto.TVShowsRemoteDto
+import com.elhady.movies.core.network.model.response.dto.TvReviewRemoteDto
+import com.elhady.movies.core.network.model.response.dto.YoutubeVideoDetailsRemoteDto
+import com.elhady.movies.core.network.model.response.dto.episode_details.EpisodeDetailsCastRemoteDto
+import com.elhady.movies.core.network.model.response.dto.episode_details.EpisodeDetailsRemoteDto
+import com.elhady.movies.core.network.model.response.dto.episode_details.RatingEpisodeDetailsRemoteDto
+import com.elhady.movies.core.network.model.response.dto.season_details.SeasonDetailsDto
+import com.elhady.movies.core.network.model.response.dto.TvDetailsCreditRemoteDto
+import com.elhady.movies.core.network.model.response.dto.TvDetailsRemoteDto
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TvShowApiService {
+
+    @GET("tv/airing_today")
+    suspend fun getAiringTodayTVShows(@Query("page") page: Int = 1): Response<DataWrapperResponse<TVShowsRemoteDto>>
+
+    @GET("tv/top_rated")
+    suspend fun getTopRatedTVShows(@Query("page") page: Int = 1): Response<DataWrapperResponse<TVShowsRemoteDto>>
+
+    @GET("tv/on_the_air")
+    suspend fun getOnTheAirTVShows(@Query("page") page: Int = 1): Response<DataWrapperResponse<TVShowsRemoteDto>>
+
+    @GET("tv/popular")
+    suspend fun getPopularTVShows(@Query("page") page: Int = 1): Response<DataWrapperResponse<TVShowsRemoteDto>>
+
+    @GET("tv/{tv_id}/videos")
+    suspend fun getTrailerVideoForTvShow(
+        @Path("tv_id") tvShowId: Int
+    ): Response<DataWrapperResponse<YoutubeVideoDetailsRemoteDto>>
+
+    @GET("tv/{series_id}/season/{season_number}/episode/{episode_number}/videos")
+    suspend fun getEpisodeVideos(
+        @Path("series_id") seriesId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int
+    ): Response<DataWrapperResponse<YoutubeVideoDetailsRemoteDto>>
+
+    @GET("tv/{series_id}/season/{season_number}")
+    suspend fun getSeasonDetails(
+        @Path("series_id") seriesId: Int,
+        @Path("season_number") seasonNumber: Int
+    ): Response<SeasonDetailsDto>
+
+    @GET("tv/{tv_id}")
+    suspend fun getTvDetails(
+        @Path("tv_id") tvShowId: Int
+    ): Response<TvDetailsRemoteDto>
+
+    @GET("tv/{tv_id}/aggregate_credits")
+    suspend fun getTvDetailsCredit(
+        @Path("tv_id") tvShowId: Int
+    ): Response<TvDetailsCreditRemoteDto>
+
+    @POST("tv/{tv_id}/rating?")
+    suspend fun rateTvShow(
+        @Body rateRequest: RateRequest, @Path("tv_id") tvShowId: Int,
+    ): Response<StatusResponse>
+
+    @GET("tv/{tv_id}/reviews")
+    suspend fun getTvShowReviews(
+        @Path("tv_id") tvShowId: Int
+    ): Response<DataWrapperResponse<TvReviewRemoteDto>>
+
+    @GET("tv/{tv_id}/recommendations")
+    suspend fun getTvShowRecommendations(
+        @Path("tv_id") tvShowId: Int
+    ): Response<DataWrapperResponse<TVShowsRemoteDto>>
+
+    @GET("tv/{series_id}/season/{season_number}/episode/{episode_number}")
+    suspend fun getEpisodeDetails(
+        @Path("series_id") seriesId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int
+    ): Response<EpisodeDetailsRemoteDto>
+
+    @Headers("Content-Type: application/json;charset=utf-8")
+    @POST("tv/{series_id}/season/{season_number}/episode/{episode_number}/rating")
+    suspend fun postEpisodeRating(
+        @Body rate: RatingEpisodeDetailsRequest,
+        @Path("series_id") seriesId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int
+    ): Response<RatingEpisodeDetailsRemoteDto>
+
+    @GET("tv/{series_id}/season/{season_number}/episode/{episode_number}/credits")
+    suspend fun getEpisodeCast(
+        @Path("series_id") seriesId: Int,
+        @Path("season_number") seasonNumber: Int,
+        @Path("episode_number") episodeNumber: Int
+    ): Response<EpisodeDetailsCastRemoteDto>
+}
