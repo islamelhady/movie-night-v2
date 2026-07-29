b6ccdd71c46541e3a8fbccff6d837b77c8baa387
islam elhady <i.elhady44@gmail.com>
2026-07-29 23:59:39 +0300

    Update PagingSource classes to use renamed domain models and replace MovieDao with GenreDao

--- core/data/src/main/java/com/elhady/movies/core/data/paging/movie/RatedMoviesPagingSource.kt
+++ core/data/src/main/java/com/elhady/movies/core/data/paging/movie/RatedMoviesPagingSource.kt
@@ -1,9 +1,9 @@
 package com.elhady.movies.core.data.paging.movie
 
-import com.elhady.movies.core.database.MovieDao
+import com.elhady.movies.core.database.dao.GenreDao
 import com.elhady.movies.core.network.api.AccountApiService
 import com.elhady.movies.core.data.base.BasePagingSource
-import com.elhady.movies.core.domain.model.account.MyRatedMovieEntity
+import com.elhady.movies.core.domain.model.account.MyRatedMovie
 import com.elhady.movies.core.data.mapper.movie.DomainGenreMapper
 import com.elhady.movies.core.data.mapper.account.DomainMyRatedMoviesMapper
 import javax.inject.Inject
@@ -12,12 +12,12 @@ class RatedMoviesPagingSource @Inject constructor(
     service: AccountApiService,
     private val domainGenreMapper: DomainGenreMapper,
     private val mapper: DomainMyRatedMoviesMapper,
-    private val movieDao: MovieDao,
-) : BasePagingSource<AccountApiService, MyRatedMovieEntity>(service) {
+    private val genreDao: GenreDao,
+) : BasePagingSource<AccountApiService, MyRatedMovie>(service) {
 
-    override suspend fun fetchData(page: Int): List<MyRatedMovieEntity> {
+    override suspend fun fetchData(page: Int): List<MyRatedMovie> {
         val response = service.getRatedMovies(page).body()?.results?.filterNotNull()
-        val genreMovieMapper = domainGenreMapper.map(movieDao.getGenresMovies())
+        val genreMovieMapper = domainGenreMapper.map(genreDao.getGenresMovies())
         return response?.map { mapper.map(it , genreMovieMapper) } ?: emptyList()
     }
 }
