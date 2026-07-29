9a44c09620b23d6462b262633800e6ff3c0b58a5
islam elhady <i.elhady44@gmail.com>
2026-07-29 23:26:19 +0300

    Rename `getAllWatchHistoryVideos` to `getAllWatchHistory` in `WatchHistoryDao`

--- core/database/src/main/java/com/elhady/movies/core/database/dao/WatchHistoryDao.kt
+++ core/database/src/main/java/com/elhady/movies/core/database/dao/WatchHistoryDao.kt
@@ -10,14 +10,14 @@ import com.elhady.movies.core.database.entity.movie.MovieInWatchHistoryEntity
 @Dao
 interface WatchHistoryDao {
 
-    @Query("select * from WATCH_HISTORY_MOVIES_TABLE")
-    suspend fun getAllWatchHistoryVideos(): List<MovieInWatchHistoryEntity>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovieToWatchHistory(movieInWatchHistoryEntity: MovieInWatchHistoryEntity)
 
     @Delete
     suspend fun deleteMovieFromWatchHistory(movieInWatchHistoryEntity: MovieInWatchHistoryEntity)
+
+    @Query("select * from WATCH_HISTORY_MOVIES_TABLE")
+    suspend fun getAllWatchHistory(): List<MovieInWatchHistoryEntity>
 
     @Query("select * from WATCH_HISTORY_MOVIES_TABLE where title like :keyword")
     suspend fun searchWatchHistory(keyword: String): List<MovieInWatchHistoryEntity>
