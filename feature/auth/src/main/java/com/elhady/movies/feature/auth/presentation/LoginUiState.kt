526109d646b6ca33e9999897d519bdfefc83c095
islam elhady <i.elhady44@gmail.com>
2026-07-30 20:51:05 +0300

    Remove login presentation components from auth feature

--- feature/auth/src/main/java/com/elhady/movies/feature/auth/presentation/LoginUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.elhady.movies.feature.auth.presentation
-
-data class LoginUiState(
-    var userName: String = "",
-    val userNameError: String? = null,
-    var password: String = "",
-    val passwordError: String? = null,
-    val isLoading: Boolean = false,
-)
