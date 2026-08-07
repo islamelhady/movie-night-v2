e89ac757684c4f37f890572994902b6358993aa7
islam elhady <i.elhady44@gmail.com>
2026-08-07 23:42:45 +0300

    Refactor AppException to use object instead of data object for specific exceptions

--- core/common/src/main/java/com/elhady/movies/core/common/AppException.kt
+++ core/common/src/main/java/com/elhady/movies/core/common/AppException.kt
@@ -1,15 +1,15 @@
 package com.elhady.movies.core.common
 
 sealed class AppException(message: String? = null) : Exception(message) {
-    data object NoNetwork : AppException()
-    data object Timeout : AppException()
-    data object Unauthorized : AppException()
-    data object Forbidden : AppException()
-    data object BadRequest : AppException()
-    data object Validation : AppException()
-    data object NotFound : AppException()
-    data object Conflict : AppException()
-    data object TooManyRequests : AppException()
-    data class Server(val code: Int, val serverMessage: String) : AppException(serverMessage)
+     object NoNetwork : AppException()
+     object Timeout : AppException()
+     object Unauthorized : AppException()
+     object Forbidden : AppException()
+     object BadRequest : AppException()
+     object Validation : AppException()
+     object NotFound : AppException()
+     object Conflict : AppException()
+     object TooManyRequests : AppException()
+     data class Server(val code: Int, val serverMessage: String) : AppException(serverMessage)
     data class Unknown(val errorMessage: String) : AppException(errorMessage)
 }
