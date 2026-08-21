bdcda672c9d3bff77c14d166a5229a7237052b03
islam elhady <i.elhady44@gmail.com>
2026-08-21 09:12:18 +0300

    Refactor TV details state management for improved clarity and consistency

--- feature/details/src/main/java/com/elhady/movies/feature/details/presentation/tvdetails/TvDetailsItem.kt
+++ feature/details/src/main/java/com/elhady/movies/feature/details/presentation/tvdetails/TvDetailsItem.kt
@@ -4,9 +4,10 @@ import com.elhady.movies.feature.details.presentation.episodedetails.CommentUiSt
 import com.elhady.movies.core.ui.state.MediaVerticalUiState
 import com.elhady.movies.core.ui.state.PeopleUiState
 import com.elhady.movies.feature.details.presentation.seasondetails.SeasonHorizontalUiState
+import com.elhady.movies.feature.details.presentation.tvdetails.state.InfoUIState
 
 sealed class TvDetailsItem(val type: TvDetailsType) {
-    data class Upper(val upperUiState: TvDetailsUiState.Info) : TvDetailsItem(TvDetailsType.UPPER)
+    data class Info(val info: InfoUIState.Info) : TvDetailsItem(TvDetailsType.INFO)
     data class People(val people: List<PeopleUiState>, val isSeasonNotEmpty: Boolean) : TvDetailsItem(
         TvDetailsType.PEOPLE
     )
@@ -19,4 +20,4 @@ sealed class TvDetailsItem(val type: TvDetailsType) {
     data class Review(val review: CommentUiState) : TvDetailsItem(TvDetailsType.REVIEWS)
 }
 
-enum class TvDetailsType { UPPER, PEOPLE, Seasons, RECOMMENDED, REVIEWS }
+enum class TvDetailsType { INFO, PEOPLE, Seasons, RECOMMENDED, REVIEWS }
--- feature/details/src/main/java/com/elhady/movies/feature/details/presentation/tvdetails/TvDetailsUiEvent.kt
+++ feature/details/src/main/java/com/elhady/movies/feature/details/presentation/tvdetails/TvDetailsUiEvent.kt
@@ -1,21 +1,22 @@
 package com.elhady.movies.feature.details.presentation.tvdetails
 
 sealed interface TvDetailsUiEvent {
-    object AddNewListEvent: TvDetailsUiEvent
-    object ClosetEvent: TvDetailsUiEvent
-    object RateTvEvent : TvDetailsUiEvent
-    data class PlayButton(val youtubeKey: String) : TvDetailsUiEvent
-    data class ApplyRating(val message: String) : TvDetailsUiEvent
-    data class OnPersonClick(val id: Int) : TvDetailsUiEvent
-    data class OnSeasonClick(val seasonNumber: Int) : TvDetailsUiEvent
-    data class OnRecommended(val id: Int) : TvDetailsUiEvent
-    object Back : TvDetailsUiEvent
-    object OnShowMoreCast : TvDetailsUiEvent
-    object OnShowMoreRecommended : TvDetailsUiEvent
-    data class ShowSnackBar(val message: String) : TvDetailsUiEvent
-    data class OnSaveButtonClick(val tvShowId: Int) : TvDetailsUiEvent
-    data class OnDoneAdding(val message: String): TvDetailsUiEvent
-    data class OnCreateNewList(val message:String): TvDetailsUiEvent
-    class OnFavourite(val message: String) : TvDetailsUiEvent
-    class OnWatchList(val message: String) : TvDetailsUiEvent
+    object BackClicked: TvDetailsUiEvent
+    object PlayClicked: TvDetailsUiEvent
+    object RateClicked: TvDetailsUiEvent
+    object SaveClicked: TvDetailsUiEvent
+    object FavouriteClicked: TvDetailsUiEvent
+    object WatchlistClicked: TvDetailsUiEvent
+    object ShowMoreCastClicked: TvDetailsUiEvent
+    object ShowMoreRecommendedClicked: TvDetailsUiEvent
+    data class PersonClicked(val personId: Int) : TvDetailsUiEvent
+    data class SeasonClicked(val seasonNumber: Int) : TvDetailsUiEvent
+    data class RecommendationClicked(val tvShowId: Int) : TvDetailsUiEvent
+    data class RatingChanged(val rating: Float) : TvDetailsUiEvent
+    object RatingSubmitted : TvDetailsUiEvent
+    data class ListSelected(val listId: Int) : TvDetailsUiEvent
+    object DoneAddingLists : TvDetailsUiEvent
+    object AddNewListClicked : TvDetailsUiEvent
+    data class CreateNewListClicked(val listName: String) : TvDetailsUiEvent
+    object Retry : TvDetailsUiEvent
 }
