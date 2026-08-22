f00f73b0180bf6f89c617e51793c7ec236e61e10
islam elhady <i.elhady44@gmail.com>
2026-08-22 16:26:58 +0300

    Refactor MovieDetails UI events and effects for improved interaction and clarity

---
+++ feature/details/src/main/java/com/elhady/movies/feature/details/presentation/moviedetails/MovieDetailsUiEffect.kt
@@ -0,0 +1,19 @@
+package com.elhady.movies.feature.details.presentation.moviedetails
+
+import com.elhady.movies.core.ui.state.UserListUiState
+
+sealed interface MovieDetailsUiEffect {
+    object NavigateBack : MovieDetailsUiEffect
+    data class NavigateToPeopleDetails(val personId: Int) : MovieDetailsUiEffect
+    data class NavigateToMovieDetails(val movieId: Int) : MovieDetailsUiEffect
+    data class ShowSnackBar(val message: String) : MovieDetailsUiEffect
+    object ShowRateBottomSheet : MovieDetailsUiEffect
+    data class ShowSaveToListBottomSheet(val lists: List<UserListUiState>) : MovieDetailsUiEffect
+    data class NavigateToShowMore(val movieId: Int) : MovieDetailsUiEffect
+    data class PlayVideoTrailer(val videoKey: String) : MovieDetailsUiEffect
+    
+    // Bottom Sheet interactions
+    object CloseBottomSheet : MovieDetailsUiEffect
+    object AddListToBottomSheet : MovieDetailsUiEffect
+    object DoneEvent : MovieDetailsUiEffect
+}
