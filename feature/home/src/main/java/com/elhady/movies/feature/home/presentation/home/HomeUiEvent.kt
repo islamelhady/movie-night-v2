8688dc76853aca80b67d28b1d8941db4cb33abe0
islam elhady <i.elhady44@gmail.com>
2026-08-23 13:26:58 +0300

    Refactor HomeItem and related classes to use sealed interfaces for better type safety and clarity

--- feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeUiEvent.kt
+++ feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeUiEvent.kt
@@ -3,8 +3,19 @@ package com.elhady.movies.feature.home.presentation.home
 import com.elhady.movies.core.common.ShowMoreType
 
 sealed interface HomeUiEvent {
-    data class MovieEvent(val itemId: Int) : HomeUiEvent
-    data class TvShowEvent(val itemId: Int) : HomeUiEvent
-    data class ClickShowMoreEvent(val showMore: ShowMoreType) : HomeUiEvent
-    data class ShowSnackBarEvent(val message: String) : HomeUiEvent
+    data class MovieClicked(
+        val movieId: Int
+    ) : HomeUiEvent
+
+    data class TvShowClicked(
+        val tvShowId: Int
+    ) : HomeUiEvent
+
+    data class PeopleClicked(
+        val personId: Int
+    ) : HomeUiEvent
+
+    data class ShowMoreClicked(
+        val type: ShowMoreType
+    ) : HomeUiEvent
 }
--- feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeUiState.kt
+++ feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeUiState.kt
@@ -1,83 +1,69 @@
 package com.elhady.movies.feature.home.presentation.home
 
-import kotlin.math.roundToInt
+import com.elhady.movies.core.ui.base.ErrorUiState
 
 data class HomeUiState(
-    val upComingMovies: List<UpComingMoviesUiState> = emptyList(),
-    val nowPlayingMovies: List<NowPlayingUiState> = emptyList(),
-    val trendingMovies: List<TrendingMoviesUiState> = emptyList(),
-    val popularPeople: List<PopularPeopleUiState> = emptyList(),
-    val popularMovies: List<PopularMoviesUiState> = emptyList(),
-    val topRated: List<TopRatedUiState> = emptyList(),
-    val tvShow: List<TvShowUiState> = emptyList(),
-    val airingTodayTvShow: List<AiringTodayTvShowUiState> = emptyList(),
-    val onErrors: List<String> = emptyList(),
+    val upcomingMovies: List<UpcomingMovieUiState> = emptyList(),
+    val nowPlayingMovies: List<NowPlayingMovieUiState> = emptyList(),
+    val trendingMovies: List<TrendingMovieUiState> = emptyList(),
+    val popularPeople: List<PopularPersonUiState> = emptyList(),
+    val popularMovies: List<PopularMovieUiState> = emptyList(),
+    val topRatedMovies: List<TopRatedMovieUiState> = emptyList(),
+    val tvShows: List<TvShowUiState> = emptyList(),
+    val airingTodayTvShows: List<AiringTodayTvShowUiState> = emptyList(),
     val isLoading: Boolean = false,
+    val error: ErrorUiState? = null,
 ) {
     val isError: Boolean
-        get() = onErrors.isNotEmpty()
+        get() = error != null
 }
 
-data class UpComingMoviesUiState(
-    val id: Int = 0,
-    val imageUrl: String = "",
-    val title: String = "",
-    val genres: List<String> = emptyList(),
-    val rate: Double = 0.0
-) {
-    fun formattedRate(): Double = (rate * 10).roundToInt() / 10.0
-}
+data class UpcomingMovieUiState(
+    val id: Int,
+    val imageUrl: String,
+    val title: String,
+    val genres: List<String>,
+    val rate: Double,
+)
 
-data class NowPlayingUiState(
+data class NowPlayingMovieUiState(
     val id: Int,
     val imageUrl: String,
     val title: String,
 )
 
-
-data class TrendingMoviesUiState(
+data class TrendingMovieUiState(
     val id: Int,
     val imageUrl: String,
-    val rate: Double
-) {
-    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
-}
-
-data class PopularPeopleUiState(
-    val id: Int,
-    val profilePath: String,
-    val name: String
+    val rate: Double,
 )
 
-data class PopularMoviesUiState(
+data class PopularPersonUiState(
+    val id: Int,
+    val profilePath: String,
+    val name: String,
+)
+
+data class PopularMovieUiState(
     val id: Int,
     val imageUrl: String,
-    val rate: Double
-) {
-    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
-}
+    val rate: Double,
+)
 
-
-data class TopRatedUiState(
+data class TopRatedMovieUiState(
     val id: Int,
     val imageUrl: String,
-    val rate: Double
-) {
-    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
-}
+    val rate: Double,
+)
 
 data class TvShowUiState(
     val id: Int,
     val imageUrl: String,
-    val rate: Double
-) {
-    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
-}
+    val rate: Double,
+)
 
 data class AiringTodayTvShowUiState(
     val id: Int,
     val imageUrl: String,
-    val rate: Double
-) {
-    fun formattedRate(): Double = (rate * 100).roundToInt() / 100.0
-}
+    val rate: Double,
+)
