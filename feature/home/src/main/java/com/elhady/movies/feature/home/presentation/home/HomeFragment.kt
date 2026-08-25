d055ec3ef91ac1d0b20d062ccbb37ef9ff255367
islam elhady <i.elhady44@gmail.com>
2026-08-25 12:22:02 +0300

    Refactor Home feature to update listener interface and method names for improved clarity and consistency

--- feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeFragment.kt
+++ feature/home/src/main/java/com/elhady/movies/feature/home/presentation/home/HomeFragment.kt
@@ -13,7 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding, HomeUiState, HomeUiEffect>(), HomeAdapterListener {
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeUiState, HomeUiEffect>(), HomeAdapterListener , HomeListener {
     @Inject
     lateinit var navigator: Navigator
 
@@ -90,11 +90,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeUiState, HomeUiEffect
         }
     }
 
-    override fun onMovieClick(id: Int) {
+    override fun onClickMovie(id: Int) {
         viewModel.onEvent(HomeUiEvent.MovieClicked(id))
     }
 
-    override fun onTvShowClick(id: Int) {
+    override fun onClickTvShow(id: Int) {
         viewModel.onEvent(HomeUiEvent.TvShowClicked(id))
     }
 
