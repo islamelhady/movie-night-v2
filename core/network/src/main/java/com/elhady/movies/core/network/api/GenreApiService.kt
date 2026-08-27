35cb647eff36fb0f21206a2ec56a5503232daf30
islam elhady <i.elhady44@gmail.com>
2026-08-27 14:26:16 +0300

    refactor: simplify genre API service methods and update language query parameter handling

--- core/network/src/main/java/com/elhady/movies/core/network/api/GenreApiService.kt
+++ core/network/src/main/java/com/elhady/movies/core/network/api/GenreApiService.kt
@@ -5,17 +5,12 @@ import com.elhady.movies.core.network.dto.common.GenreMovieDto
 import com.elhady.movies.core.network.dto.common.GenreTvDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface GenreApiService {
 
     @GET("genre/movie/list")
-    suspend fun getListOfGenresForMovies(
-        @Query("language") language: String = "en"
-    ): Response<GenresWrapperResponse<GenreMovieDto>>
+    suspend fun getListOfGenresForMovies(): Response<GenresWrapperResponse<GenreMovieDto>>
 
     @GET("genre/tv/list")
-    suspend fun getListOfGenresForTvs(
-        @Query("language") language: String = "en"
-    ): Response<GenresWrapperResponse<GenreTvDto>>
+    suspend fun getListOfGenresForTvs(): Response<GenresWrapperResponse<GenreTvDto>>
 }
